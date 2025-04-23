7553d622a92cdd1e47408e651c7c4ddc4c70eff6
sakai1250 <200442066@ccalumni.meijo-u.ac.jp>
2025-04-23 22:43:54 +0900

    change: model

--- day1/03_FastAPI/app.py
+++ day1/03_FastAPI/app.py
@@ -13,7 +13,8 @@ from pyngrok import ngrok
 
 # --- 設定 ---
 # モデル名を設定
-MODEL_NAME = "google/gemma-2-2b-jpn-it"  # お好みのモデルに変更可能です
+MODEL_NAME = "rinna/japanese-gpt-neox-3.6b-instruction-ppo"
+#MODEL_NAME = "google/gemma-2-2b-jpn-it"  # お好みのモデルに変更可能です
 print(f"モデル名を設定: {MODEL_NAME}")
 
 # --- モデル設定クラス ---
